f26ec49e46f5217189e2b25347e56b7c29cacacb
Josh de Leeuw <josh.deleeuw@gmail.com>
2016-04-08 16:20:00 -0400

    clean up and organization

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
 Data &amp; analysis files from de Leeuw, Andrews, Livingston, and Chin (in prep).
-
-To run, open the Rproj file with RStudio and run the file `common_dv_scale.R` in the
-`data & analysis` folder.
--- data & analysis/bayes_factor_model_comparison_dimension.R
+++ data & analysis/bayes_factor_model_comparison_dimension.R
@@ -2,113 +2,116 @@
 require(BayesFactor)
 
 # load data for dimension analysis ####
-bf_dimension <- read.csv("data & analysis/export-data/bf-dimension.csv")
-bf_dimension$mturk_id <- factor(bf_dimension$mturk_id)
+bf.dimension.data <- read.csv("data & analysis/export-data/bf-dimension.csv")
+bf.dimension.data$mturk_id <- factor(bf.dimension.data$mturk_id)
+
+# main effects ####
+bf.dimension.stim_type <- lmBF(z ~ stim_type + mturk_id, whichRandom = c('mturk_id'), data=bf.dimension.data)
+bf.dimension.train_type <- lmBF(z ~ train_type + mturk_id, whichRandom = c('mturk_id'), data=bf.dimension.data)
+bf.dimension.dimension <- lmBF(z ~ dimension + mturk_id, whichRandom = c('mturk_id'), data=bf.dimension.data)
+bf.dimension.varying_dimension <- lmBF(z ~ varying_dimension + mturk_id, whichRandom = c('mturk_id'), data=bf.dimension.data)
+bf.dimension.intercept.only <- lmBF(z ~ mturk_id, whichRandom = c('mturk_id'), data=bf.dimension.data)
+bf.dimension.main.effects.only <- c(
+  bf.dimension.stim_type/bf.dimension.intercept.only,
+  bf.dimension.train_type/bf.dimension.intercept.only,
+  bf.dimension.dimension/bf.dimension.intercept.only,
+  bf.dimension.varying_dimension/bf.dimension.intercept.only
+)
+summary(bf.dimension.main.effects.only)
 
 # model comparison for CP effect ####
-main.effects <- 'z ~ stim_type + train_type + dimension + varying_dimension + trial_type + mturk_id'
-main.effects.cp <- 'z ~ stim_type + train_type + dimension + varying_dimension + trial_type + mturk_id + train_type:varying_dimension'
+dimension.main.effects <- 'z ~ stim_type + train_type + dimension + varying_dimension + trial_type + mturk_id'
+dimension.main.effects.cp <- 'z ~ stim_type + train_type + dimension + varying_dimension + trial_type + mturk_id + train_type:varying_dimension'
 
-bf.main <- lmBF(as.formula(main.effects), whichRandom = c('mturk_id'), data=bf_dimension)
-bf.cp <- lmBF(as.formula(main.effects.cp), whichRandom = c('mturk_id'), data=bf_dimension)
+bf.dimension.main <- lmBF(as.formula(dimension.main.effects), whichRandom = c('mturk_id'), data=bf.dimension.data)
+bf.dimension.cp <- lmBF(as.formula(dimension.main.effects.cp), whichRandom = c('mturk_id'), data=bf.dimension.data)
+bf.dimension.cp.effect <- bf.dimension.cp / bf.dimension.main
+summary(bf.dimension.cp.effect)
 
-bf.cp.effect <- bf.cp / bf.main
-summary(bf.cp.effect) # CP model is preferred 200:1 over main effects model.
-
-# testing main effects
-bf.stim_type <- lmBF(z ~ stim_type + mturk_id, whichRandom = c('mturk_id'), data=bf_dimension)
-bf.train_type <- lmBF(z ~ train_type + mturk_id, whichRandom = c('mturk_id'), data=bf_dimension)
-bf.dimension <- lmBF(z ~ dimension + mturk_id, whichRandom = c('mturk_id'), data=bf_dimension)
-bf.varying_dimension <- lmBF(z ~ varying_dimension + mturk_id, whichRandom = c('mturk_id'), data=bf_dimension)
-bf.intercept.only <- lmBF(z ~ mturk_id, whichRandom = c('mturk_id'), data=bf_dimension)
-bf.main.effects.only <- c(bf.stim_type/bf.intercept.only,bf.train_type/bf.intercept.only,bf.dimension/bf.intercept.only,bf.varying_dimension/bf.intercept.only)
-summary(bf.main.effects.only)
-
-# model comparisons for trial_type effect ####
+# model comparisons for trial_type X CP interactions ####
 # the main goal of this analysis is to see if models with trial_type interacting with the CP interaction (train_type:varying_dimension)
 # are ever favored over the model with just the main effects and the CP interaction.
-a <- 'trial_type:train_type:varying_dimension'
-b <- 'trial_type:train_type:varying_dimension:dimension'
-c <- 'trial_type:train_type:varying_dimension:stim_type'
-d <- 'trial_type:train_type:varying_dimension:stim_type:dimension'
+dimension.trial_type.a <- 'trial_type:train_type:varying_dimension'
+dimension.trial_type.b <- 'trial_type:train_type:varying_dimension:dimension'
+dimension.trial_type.c <- 'trial_type:train_type:varying_dimension:stim_type'
+dimension.trial_type.d <- 'trial_type:train_type:varying_dimension:stim_type:dimension'
 
-models.trial_type = c(
-  paste(main.effects.cp, a, sep = ' + '),
-  paste(main.effects.cp, b, sep = ' + '),
-  paste(main.effects.cp, c, sep = ' + '),
-  paste(main.effects.cp, d, sep = ' + '),
-  paste(main.effects.cp, a, b, sep = ' + '),
-  paste(main.effects.cp, a, c, sep = ' + '),
-  paste(main.effects.cp, a, d, sep = ' + '),
-  paste(main.effects.cp, b, c, sep = ' + '),
-  paste(main.effects.cp, b, d, sep = ' + '),
-  paste(main.effects.cp, c, d, sep = ' + '),
-  paste(main.effects.cp, a, b, c, sep = ' + '),
-  paste(main.effects.cp, a, b, d, sep = ' + '),
-  paste(main.effects.cp, a, c, d, sep = ' + '),
-  paste(main.effects.cp, b, c, d, sep = ' + '),
-  paste(main.effects.cp, a, b, c, d, sep = ' + ')
+dimension.models.trial_type = c(
+  paste(dimension.main.effects.cp, dimension.trial_type.a, sep = ' + '),
+  paste(dimension.main.effects.cp, dimension.trial_type.b, sep = ' + '),
+  paste(dimension.main.effects.cp, dimension.trial_type.c, sep = ' + '),
+  paste(dimension.main.effects.cp, dimension.trial_type.d, sep = ' + '),
+  paste(dimension.main.effects.cp, dimension.trial_type.a, dimension.trial_type.b, sep = ' + '),
+  paste(dimension.main.effects.cp, dimension.trial_type.a, dimension.trial_type.c, sep = ' + '),
+  paste(dimension.main.effects.cp, dimension.trial_type.a, dimension.trial_type.d, sep = ' + '),
+  paste(dimension.main.effects.cp, dimension.trial_type.b, dimension.trial_type.c, sep = ' + '),
+  paste(dimension.main.effects.cp, dimension.trial_type.b, dimension.trial_type.d, sep = ' + '),
+  paste(dimension.main.effects.cp, dimension.trial_type.c, dimension.trial_type.d, sep = ' + '),
+  paste(dimension.main.effects.cp, dimension.trial_type.a, dimension.trial_type.b, dimension.trial_type.c, sep = ' + '),
+  paste(dimension.main.effects.cp, dimension.trial_type.a, dimension.trial_type.b, dimension.trial_type.d, sep = ' + '),
+  paste(dimension.main.effects.cp, dimension.trial_type.a, dimension.trial_type.c, dimension.trial_type.d, sep = ' + '),
+  paste(dimension.main.effects.cp, dimension.trial_type.b, dimension.trial_type.c, dimension.trial_type.d, sep = ' + '),
+  paste(dimension.main.effects.cp, dimension.trial_type.a, dimension.trial_type.b, dimension.trial_type.c, dimension.trial_type.d, sep = ' + ')
 )
 
-bf.results.trial_type <- lapply(models.trial_type, function(x){
-  bf <- lmBF(as.formula(x), whichRandom = c('mturk_id'), data=bf_dimension)
+bf.dimension.results.trial_type <- lapply(dimension.models.trial_type, function(x){
+  bf <- lmBF(as.formula(x), whichRandom = c('mturk_id'), data=bf.dimension.data)
   return(bf)
 })
 
-bf.trial_type.against.cp <- lapply(bf.results.trial_type, function(x){
-  bf <- x / bf.cp
+bf.dimension.trial_type.against.cp <- lapply(bf.dimension.results.trial_type, function(x){
+  bf <- x / bf.dimension.cp
   return(bf)
 })
 
-bf.combined.trial_type <- bf.trial_type.against.cp[[1]]
-for(i in 2:length(bf.trial_type.against.cp)){
-  bf.combined.trial_type <- c(bf.combined.trial_type, bf.trial_type.against.cp[[i]])
+bf.dimension.combined.trial_type <- bf.dimension.trial_type.against.cp[[1]]
+for(i in 2:length(bf.dimension.trial_type.against.cp)){
+  bf.dimension.combined.trial_type <- c(bf.dimension.combined.trial_type, bf.dimension.trial_type.against.cp[[i]])
 }
 
-plot(bf.combined.trial_type, pars=par(cex.axis=0.1),marginExpand = 0.1)
+# model comparisons for stim_type X CP interactions ####
 
-# model comparisons for stim_type effect ####
+dimension.stim_type.a <- 'stim_type:train_type:varying_dimension'
+dimension.stim_type.b <- 'stim_type:train_type:varying_dimension:dimension'
+dimension.stim_type.c <- 'stim_type:train_type:varying_dimension:trial_type'
+dimension.stim_type.d <- 'stim_type:train_type:varying_dimension:trial_type:dimension'
 
-a <- 'stim_type:train_type:varying_dimension'
-b <- 'stim_type:train_type:varying_dimension:dimension'
-c <- 'stim_type:train_type:varying_dimension:trial_type'
-d <- 'stim_type:train_type:varying_dimension:trial_type:dimension'
-
-models.stim_type = c(
-  paste(main.effects.cp, a, sep = ' + '),
-  paste(main.effects.cp, b, sep = ' + '),
-  paste(main.effects.cp, c, sep = ' + '),
-  paste(main.effects.cp, d, sep = ' + '),
-  paste(main.effects.cp, a, b, sep = ' + '),
-  paste(main.effects.cp, a, c, sep = ' + '),
-  paste(main.effects.cp, a, d, sep = ' + '),
-  paste(main.effects.cp, b, c, sep = ' + '),
-  paste(main.effects.cp, b, d, sep = ' + '),
-  paste(main.effects.cp, c, d, sep = ' + '),
-  paste(main.effects.cp, a, b, c, sep = ' + '),
-  paste(main.effects.cp, a, b, d, sep = ' + '),
-  paste(main.effects.cp, a, c, d, sep = ' + '),
-  paste(main.effects.cp, b, c, d, sep = ' + '),
-  paste(main.effects.cp, a, b, c, d, sep = ' + ')
+dimension.models.stim_type = c(
+  paste(dimension.main.effects.cp, dimension.stim_type.a, sep = ' + '),
+  paste(dimension.main.effects.cp, dimension.stim_type.b, sep = ' + '),
+  paste(dimension.main.effects.cp, dimension.stim_type.c, sep = ' + '),
+  paste(dimension.main.effects.cp, dimension.stim_type.d, sep = ' + '),
+  paste(dimension.main.effects.cp, dimension.stim_type.a, dimension.stim_type.b, sep = ' + '),
+  paste(dimension.main.effects.cp, dimension.stim_type.a, dimension.stim_type.c, sep = ' + '),
+  paste(dimension.main.effects.cp, dimension.stim_type.a, dimension.stim_type.d, sep = ' + '),
+  paste(dimension.main.effects.cp, dimension.stim_type.b, dimension.stim_type.c, sep = ' + '),
+  paste(dimension.main.effects.cp, dimension.stim_type.b, dimension.stim_type.d, sep = ' + '),
+  paste(dimension.main.effects.cp, dimension.stim_type.c, dimension.stim_type.d, sep = ' + '),
+  paste(dimension.main.effects.cp, dimension.stim_type.a, dimension.stim_type.b, dimension.stim_type.c, sep = ' + '),
+  paste(dimension.main.effects.cp, dimension.stim_type.a, dimension.stim_type.b, dimension.stim_type.d, sep = ' + '),
+  paste(dimension.main.effects.cp, dimension.stim_type.a, dimension.stim_type.c, dimension.stim_type.d, sep = ' + '),
+  paste(dimension.main.effects.cp, dimension.stim_type.b, dimension.stim_type.c, dimension.stim_type.d, sep = ' + '),
+  paste(dimension.main.effects.cp, dimension.stim_type.a, dimension.stim_type.b, dimension.stim_type.c, dimension.stim_type.d, sep = ' + ')
 )
 
-bf.results.stim_type <- lapply(models.stim_type, function(x){
-  bf <- lmBF(as.formula(x), whichRandom = c('mturk_id'), data=bf_dimension)
+bf.dimension.results.stim_type <- lapply(dimension.models.stim_type, function(x){
+  bf <- lmBF(as.formula(x), whichRandom = c('mturk_id'), data=bf.dimension.data)
   return(bf)
 })
 
-bf.stim_type.against.cp <- lapply(bf.results.stim_type, function(x){
-  bf <- x / bf.cp
+bf.dimension.stim_type.against.cp <- lapply(bf.dimension.results.stim_type, function(x){
+  bf <- x / bf.dimension.cp
   return(bf)
 })
 
-bf.combined.stim_type <- bf.stim_type.against.cp[[1]]
-for(i in 2:length(bf.stim_type.against.cp)){
-  bf.combined.stim_type <- c(bf.combined.stim_type, bf.stim_type.against.cp[[i]])
+bf.dimension.combined.stim_type <- bf.dimension.stim_type.against.cp[[1]]
+for(i in 2:length(bf.dimension.stim_type.against.cp)){
+  bf.dimension.combined.stim_type <- c(bf.dimension.combined.stim_type, bf.dimension.stim_type.against.cp[[i]])
 }
 
-plot(bf.combined.stim_type, pars=par(cex.axis=0.1), marginExpand = 0.1)
+# check if dimension interacted with CP ####
+bf.dimension.cp.dimension <- lmBF(z ~ stim_type + train_type + dimension + varying_dimension + trial_type + mturk_id + train_type:varying_dimension + train_type:varying_dimension:dimension, whichRandom = c('mturk_id'),data=bf.dimension.data)/bf.dimension.cp
 
-### check if dimension interacted with CP ####
-bf.cp.dimension <- lmBF(z ~ stim_type + train_type + dimension + varying_dimension + trial_type + mturk_id + train_type:varying_dimension + train_type:varying_dimension:dimension, whichRandom = c('mturk_id'),data=bf_dimension)
-summary(bf.cp.dimension/bf.cp)
+# save output! ####
+save(bf.dimension.main.effects.only, bf.dimension.cp.dimension, bf.dimension.combined.stim_type, bf.dimension.combined.trial_type, bf.dimension.cp.effect, file="data & analysis/export-data/bayes-factor-analysis-results-dimension.Rdata")
+
